109b3e65d83be749796232c4b680dad25e1f8e61
jcisking <krisloeffler@gmail.com>
2022-11-09 00:46:44 -0700

    second commit

--- app/src/main/java/com/example/composequadrant/MainActivity.kt
+++ app/src/main/java/com/example/composequadrant/MainActivity.kt
@@ -9,11 +9,15 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composequadrant.ui.theme.ComposeQuadrantTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,24 +38,85 @@ fun Greeting(name: String) {
 fun SomeFunction() {
     Column(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
         Row(modifier = Modifier.weight(1f)) {
-            Column(modifier = Modifier.weight(1f).background(Color.Green).fillMaxHeight()) {
-                Text(text = stringResource(R.string.TextComposableHeader))
-                Text(text = stringResource(id = R.string.TextComposableParagraph))
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                        .weight(1f)
+                        .background(Color.Green)
+                        .fillMaxHeight()
+                        .padding(16.dp)
+            ) {
+                Text(
+                    fontWeight = FontWeight.Bold,
+                    text = stringResource(R.string.TextComposableHeader),
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.TextComposableParagraph),
+                    textAlign = TextAlign.Justify
+                )
             }
-            Column(modifier = Modifier.weight(1f).background(Color.Yellow).fillMaxHeight()) {
-                Text(text = stringResource(R.string.ImageComposableHeader))
-                Text(text = stringResource(id = R.string.ImageComposableParagraph))
+            Column(
+                modifier = Modifier
+                        .weight(1f)
+                        .background(Color.Yellow)
+                        .fillMaxHeight()
+                        .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                Text(
+                    modifier = Modifier.padding(bottom = 16.dp) ,
+                    text = stringResource(R.string.ImageComposableHeader),
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.ImageComposableParagraph),
+                    textAlign = TextAlign.Justify
+                )
             }
 
         }
         Row(modifier = Modifier.weight(1f)) {
-            Column(modifier = Modifier.weight(1f).background(Color.Cyan).fillMaxHeight()) {
-                Text(text = stringResource(R.string.RowComposableHeader))
-                Text(text = stringResource(id = R.string.RowComposableParagraph))
+            Column(
+                modifier = Modifier
+                        .weight(1f)
+                        .background(Color.Cyan)
+                        .fillMaxHeight()
+                        .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.RowComposableHeader),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.RowComposableParagraph),
+                    textAlign = TextAlign.Justify
+                )
             }
-            Column(modifier = Modifier.weight(1f).background(Color.LightGray).fillMaxHeight()) {
-                Text(text = stringResource(R.string.ColumnComposableHeader))
-                Text(text = stringResource(id = R.string.ColumComposableParagraph))
+            Column(
+                modifier = Modifier
+                        .weight(1f)
+                        .background(Color.LightGray)
+                        .fillMaxHeight()
+                        .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = stringResource(R.string.ColumnComposableHeader),
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = stringResource(id = R.string.ColumComposableParagraph),
+                    textAlign = TextAlign.Justify
+                )
             }
         }
         
